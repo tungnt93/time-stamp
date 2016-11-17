12a242324e3fc19e361ead75b5d5f10f7a92f075
tungnt93 <tungnt93@gmail.com>
2016-11-17 20:54:04 +0700

    update commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log('App listening on', app.address().port);
+app.listen(app.get('port'), function(){
+  console.log('App listening on', app.get('port'));
 });
