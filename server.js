94449a0e5f0017d217039e36ad91e740db13799e
tungnt93 <tungnt93@gmail.com>
2016-11-18 22:30:40 +0700

    full

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile( __dirname + "/" + "index.html" );
 });
 
 app.get('/:str', function (req, res) {
