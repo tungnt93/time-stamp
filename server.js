ca3f69fe3ae7059bb6c8ad84f9aa9036a85535af
tungnt93 <tungnt93@gmail.com>
2016-11-18 22:25:16 +0700

    full

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
+
 app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/:str', function (req, res) {
+	var str = req.params['str'];
+	var response;
+	
+
+	if(Date.parse(str + " 00:00:00 GMT")){
+		var timeUnix = Date.parse(str + " 00:00:00 GMT")/1000;
+		var timestamp = moment.unix(timeUnix).add(1, 'year').format('LL');
+		response = {
+			unix: timeUnix,
+			natural: timestamp
+		}
+		res.send(response);
+	}
+	else if(moment.unix(str)){
+		var timestamp = moment.unix(str).add(1, 'year').format('LL');
+		var timeUnix = Date.parse(timestamp)/1000;
+		response = {
+			unix: timeUnix,
+			natural: timestamp
+		}
+		res.send(response);
+	}
+	else{
+		res.send({
+			unix: null,
+			natural: null
+		});
+	}
+	
+	// 
 });
 
 app.listen(app.get('port'), function(){
